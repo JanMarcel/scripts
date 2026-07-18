684eb960a49d2d2e943f1f2ca81d081897da2211
Jan-Marcel Schmidt <jani03schmidt@gmail.com>
2026-07-18 18:33:57 +0200

    delete duplicates

--- video_to_images.bash
+++
@@ -1,5 +0,0 @@
-abspath_input=$(realpath $1)
-folder_path=${abspath_input%.*}
-folder_path_with_suffix="$folder_path"_images
-mkdir -p $folder_path_with_suffix
-ffmpeg -i $1 -qscale:v 1 -qmin 1 -vf fps=5 $folder_path_with_suffix/%04d.jpg
