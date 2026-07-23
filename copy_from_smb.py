448025123612a2379471c3610620868e81ec73d1
Jan-Marcel Schmidt <jani03schmidt@gmail.com>
2026-07-23 13:03:48 +0000

    standard main form; error handling

--- copy_from_smb.py
+++ copy_from_smb.py
@@ -1,19 +1,36 @@
 import os
+import sys
 import shutil
 import smbclient
 import time
 from pathlib import Path
 from dotenv import load_dotenv
+import traceback
 
-load_dotenv()
 
-smbclient.ClientConfig(username=os.environ["USERNAME"], password=os.environ["PASSWORD"])
+def main():
+    load_dotenv()
+    
+    try:
+        username = os.environ["USERNAME"]
+        password = os.environ["PASSWORD"]
 
-zip_file = r"images_360deg_heights.zip"
-folder = r"Dobot_3D_Scan"
-remote = os.path.join(r"\\ei-data.ei.htwg-konstanz.de\ei-alle", folder, zip_file)
-local = os.path.join(".", zip_file)
+    except KeyError as e:
+        traceback.print_exc()
+        print("USERNAME and PASSWORD must be set in file called .env!")
+        sys.exit(1)
+            
+    smbclient.ClientConfig(username=username, password=password)
 
-with smbclient.open_file(remote, "rb") as remote_fh:
-    with open(local, "wb") as local_fh:
-        shutil.copyfileobj(remote_fh, local_fh)
+    zip_file = r"images_360deg_heights.zip"
+    folder = r"Dobot_3D_Scan"
+    remote = os.path.join(r"\\ei-data.ei.htwg-konstanz.de\ei-alle", folder, zip_file)
+    local = os.path.join(".", zip_file)
+
+    with smbclient.open_file(remote, "rb") as remote_fh:
+        with open(local, "wb") as local_fh:
+            shutil.copyfileobj(remote_fh, local_fh)
+
+if __name__ == '__main__':
+    main()
+
